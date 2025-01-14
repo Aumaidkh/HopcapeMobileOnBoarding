2a0fa606d48ad6f3a987673a1440ffe8fbda006d
aumaidKh <aumaidm.m.c@gmail.com>
2025-01-14 12:55:31 +0530

    Add tests for OnBoardingThemeBuilder

--- onBoardingLibrary/src/commonMain/kotlin/com/hopcape/api/theme/OnBoardingThemeBuilder.kt
+++ onBoardingLibrary/src/commonMain/kotlin/com/hopcape/api/theme/OnBoardingThemeBuilder.kt
@@ -2,10 +2,23 @@ package com.hopcape.api.theme
 
 import androidx.compose.ui.graphics.Color
 
+/**
+ * Builder class for creating and customizing an instance of [OnBoardingTheme].
+ * This class provides methods to set various theme attributes like background color,
+ * primary color, and secondary color, allowing customization of the onboarding theme.
+ *
+ * @author Murtaza Khursheed
+ */
 class OnBoardingThemeBuilder {
 
     private var onBoardingTheme = DefaultLightTheme()
 
+    /**
+     * Sets the background color of the onboarding theme.
+     *
+     * @param color The [Color] to set as the background color.
+     * @return The current instance of [OnBoardingThemeBuilder] for chaining calls.
+     */
     fun setBackgroundColor(color: Color) =
         apply {
             onBoardingTheme = onBoardingTheme.copy(
@@ -13,6 +26,12 @@ class OnBoardingThemeBuilder {
             )
         }
 
+    /**
+     * Sets the primary color of the onboarding theme.
+     *
+     * @param color The [Color] to set as the primary color.
+     * @return The current instance of [OnBoardingThemeBuilder] for chaining calls.
+     */
     fun setPrimaryColor(color: Color) =
         apply {
             onBoardingTheme = onBoardingTheme.copy(
@@ -20,6 +39,12 @@ class OnBoardingThemeBuilder {
             )
         }
 
+    /**
+     * Sets the primary color of the onboarding theme using an ARGB long value.
+     *
+     * @param color The ARGB long value representing the primary color.
+     * @return The current instance of [OnBoardingThemeBuilder] for chaining calls.
+     */
     fun setPrimaryColorArgb(color: Long) =
         apply {
             onBoardingTheme = onBoardingTheme.copy(
@@ -27,13 +52,24 @@ class OnBoardingThemeBuilder {
             )
         }
 
-    fun setSecondaryColor( color: Color ) =
+    /**
+     * Sets the secondary color of the onboarding theme.
+     *
+     * @param color The [Color] to set as the secondary color.
+     * @return The current instance of [OnBoardingThemeBuilder] for chaining calls.
+     */
+    fun setSecondaryColor(color: Color) =
         apply {
             onBoardingTheme = onBoardingTheme.copy(
                 secondaryColor = color
             )
         }
 
+    /**
+     * Builds and returns the configured [OnBoardingTheme].
+     *
+     * @return The [OnBoardingTheme] instance with the applied customizations.
+     */
     fun build(): OnBoardingTheme {
         return onBoardingTheme
     }
