c8a54c2de099a71d824183924761a7e7527756c9
aumaidKh <aumaidm.m.c@gmail.com>
2025-01-14 13:51:18 +0530

    Added KDocs

--- onBoardingLibrary/src/commonMain/kotlin/com/hopcape/onboarding/presentation/viewmodel/OnBoardingScreenState.kt
+++ onBoardingLibrary/src/commonMain/kotlin/com/hopcape/onboarding/presentation/viewmodel/OnBoardingScreenState.kt
@@ -2,9 +2,18 @@ package com.hopcape.onboarding.presentation.viewmodel
 
 import com.hopcape.api.page.OnBoardingPage
 
+/**
+ * Represents the state of the onboarding screen.
+ *
+ * This class holds the current state of the onboarding process, including the list of onboarding pages
+ * and the current page number. It is used to manage and display the onboarding flow.
+ *
+ * @property pages A list of [OnBoardingPage] objects representing the pages to be displayed in the onboarding process.
+ * @property pageNumber The index of the current page being viewed in the onboarding flow.
+ *
+ * @author Murtaza Khursheed
+ */
 data class OnBoardingScreenState(
     val pages: List<OnBoardingPage> = emptyList(),
     val pageNumber: Int = 0,
-){
-    val totalPages = pages.size
-}
+)
